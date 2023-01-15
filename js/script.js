0fb55cb6c430c9f37307efe3e3e7d3f0d69f547b
chidigit config --global user.name chidi <chidimathayas@gmail.com>
2023-01-15 02:53:19 +0100

    Fourteenth Commit

--- js/script.js
+++ js/script.js
@@ -5,40 +5,74 @@ const addBtn = document.querySelector('.add');
 const textArea = document.querySelector('.text-area');
 const textContainer = document.querySelector('.textArea');
 const save = document.querySelector('#save');
+const mainNote = document.querySelector('.note-main-container')
 
 
+// addBtn.addEventListener('click', () =>{
+//   if (textArea.value !== '') {
+
+//   }
+// })
 
 addBtn.addEventListener('click', () =>{
     textArea.style.display = 'block'
     textContainer.style.display = 'block'
 })
 
-let notes = JSON.parse(localStorage.getItem('note'));
+save.addEventListener('click', () =>{
+  textArea.style.display = 'none'
+  if (textArea.value !== '') {
+    let newItem = document.createElement('div');
+    newItem.classList.add('note-container')
+    newItem.innerHTML = `
+    <div class="note-title">
+            <P>${textArea.value}</P>
+          </div>
+       </div>
+    `
+    mainNote.appendChild(newItem)
+    textArea.value = ''
+  } else {
+    alert('Please enter a note')
+  }
+})
 
-if (notes) {
-  notes.forEach((note) =>{
-    addNote(note)
-  })
-}
-
-function addNote( text = '' ) {
-  const note = document.createElement('div')
-  note.classList.add('note')
-  textArea.value = text
-  note.innerHTML = text
-  text = push(notes)
 
 
-  textArea.addEventListener('input', (e) => {
-    const { value } = e.target.
-    note.innerHTML = value
-  })
-  updateLoacalStorage()
-}
 
-function updateLoacalStorage() {
+
+
+
+
+
+
+
+// let notes = JSON.parse(localStorage.getItem('note'));
+
+// if (notes) {
+//   notes.forEach((note) =>{
+//     addNote(note)
+//   })
+// }
+
+// function addNote( text = '' ) {
+//   const note = document.createElement('div')
+//   note.classList.add('note')
+//   textArea.value = text
+//   note.innerHTML = text
+//   text = push(notes)
+
+
+//   textArea.addEventListener('input', (e) => {
+//     const { value } = e.target.
+//     note.innerHTML = value
+//   })
+//   updateLoacalStorage()
+// }
+
+// function updateLoacalStorage() {
   
-}
+// }
 
 
 
